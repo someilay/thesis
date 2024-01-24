5d395e15502db3b9171d0887b9d4094753f31c3c
someilay <ismilioshyn@gmail.com>
2024-01-24 22:33:00 +0300

    udwadia control updates

--- src/mujoco/udwadia_control/docker_run.sh
+++ src/mujoco/udwadia_control/docker_run.sh
@@ -1,1 +1,4 @@
-docker run -i -t -e DISPLAY=unix$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix udwadia
+# /bin/bash
+
+xhost +local:
+docker run -it -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/dri/card0:/dev/dri/card0 --network host udwadia
